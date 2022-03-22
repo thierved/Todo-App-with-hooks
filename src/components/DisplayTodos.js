7c0c63f18337a6fc978957289d02e1a92de530d5
Cherno Sadu Jallow <thierved01@gmail.com>
2022-03-22 21:24:06 +0000

    reimplemented todo object

--- src/components/DisplayTodos.js
+++ src/components/DisplayTodos.js
@@ -1,7 +1,7 @@
 export default function DisplayTodos({todos}) {
     const showTodos = (todos) => {
-        if(!todos.length < 1) {
-            todos.map(todo => <li key={todo.id}>{todo.text}</li>);
+        if(todos.length !== 0) {
+            return todos.map(todo => <li key={todo.id}>{todo.text} - {todo.date}</li>);
         }
     }
     return (
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
-export default function TodoForm({todos, setTodos}) {
-    const addTodo = e => {
-        e.preventDefault();
-    };
+export default function TodoForm({ todos, setTodos }) {
+  const [date, setDate] = useState(new Date().toUTCString());
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: new Date().getUTCMilliseconds(),
+        text: e.target.elements.todo.value,
+        date: date
+      },
+    ]);
+  };
 
-    return (
-        <div className="todo-form" onSubmit={addTodo}>
-            <form>
-                <input id="todo" type="text" placeholder="Enter todo" />
-                <button>Add</button>
-            </form>
-        </div>
-    );
+  const changeDate = e => {
+      setDate(e.target.value);
+  }
+
+  return (
+    <form className="todo-form" onSubmit={addTodo}>
+      <input id="todo" type="text" placeholder="Enter todo" />
+      <input id="date" type="date" value={date} onChange={changeDate}/>
+      <button>Add</button>
+    </form>
+  );
 }
